32695472e2abd3909c6a564fdf220f05962b79f9
viniciussdahmer <viniciussteindahmer@gmail.com>
2018-05-29 22:28:13 -0300

    Added solution to "Welcome to Java!" exercise.

---
+++ Welcome to Java!/Solution.java
@@ -0,0 +1,9 @@
+// Author: Vinícius Stein Dahmer
+// Github: https://github.com/viniciussdahmer/
+
+public class Solution {
+    public static void main(String[] args) {
+      System.out.println("Hello, World.");
+      System.out.println("Hello, Java.");
+    }
+}
