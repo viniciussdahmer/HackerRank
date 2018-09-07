d81c4114af626dcf47be39921e88ff76dce399f4
viniciussdahmer <viniciussteindahmer@gmail.com>
2018-09-07 11:57:43 -0300

    Added solution to the Java Currency Formatter exercise

---
+++ Introduction/Java Currency Formatter/Solution.java
@@ -0,0 +1,22 @@
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main (String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double payment = scan.nextDouble();
+
+        Locale localeIndia = new Locale("en", "IN");
+
+        NumberFormat us = NumberFormat.getCurrencyInstance(Locale.US);
+        NumberFormat india = NumberFormat.getCurrencyInstance(localeIndia);
+        NumberFormat china = NumberFormat.getCurrencyInstance(Locale.CHINA);
+        NumberFormat france = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+
+        System.out.println("US: " + us.format(payment));
+        System.out.println("India: " + india.format(payment));
+        System.out.println("China: " + china.format(payment));
+        System.out.println("France: " + france.format(payment));
+    }
+}
