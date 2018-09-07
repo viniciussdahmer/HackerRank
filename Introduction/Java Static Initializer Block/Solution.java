09c633a45b2f62db8f3ff9332296ee66ece9e899
viniciussdahmer <viniciussteindahmer@gmail.com>
2018-09-07 10:40:30 -0300

    Added solution to the Java Static Initializer Block exercise

---
+++ Introduction/Java Static Initializer Block/Solution.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Solution {
+    static int B;
+    static int H;
+    static boolean flag;
+
+    static {
+        Scanner scan = new Scanner(System.in);
+        B = scan.nextInt();
+        H = scan.nextInt();
+        if (B <=  0 || H <=  0) {
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+            flag = false;
+        } else {
+            flag = true;
+        }
+        scan.close();
+    }
+
+    public static void main(String[] args){
+		if(flag){
+			int area=B*H;
+			System.out.print(area);
+		}
+		
+	}//end of main
+
+}//end of class
